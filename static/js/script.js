d34d20cedd46f7ef1645fbff97dc5edf006c8e23
vinicius dias <viniciusdiasvieira122@hotmail.com>
2023-10-10 01:38:39 -0300

    CSS update
    simplesmente não cosnigo fazer o js quebrar a linha

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,43 @@ document.getElementById('scrollLink').addEventListener('click', function (e) {
         behavior: 'smooth'
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const titles = [
+        "Esta sem ideias de filmes para assistir?",
+        "Descubra aqui qual filme se encaixa no seu padrão",
+        "Seu Recomendador de filmes online"
+    ];
+
+    let currentTitleIndex = 0;
+    const titleElement = document.getElementById("movie-title");
+
+    function typeTitle() {
+        let text = titles[currentTitleIndex];
+        let charIndex = 0;
+        const typeInterval = setInterval(function() {
+            if (charIndex < text.length) {
+                titleElement.textContent += text.charAt(charIndex);
+                charIndex++;
+            } else {
+                clearInterval(typeInterval);
+                setTimeout(eraseTitle, 2000);
+            }
+        }, 100);
+    }
+
+    function eraseTitle() {
+        const eraseInterval = setInterval(function() {
+            if (titleElement.textContent.length > 0) {
+                titleElement.textContent = titleElement.textContent.slice(0, -1);
+            } else {
+                clearInterval(eraseInterval);
+                currentTitleIndex = (currentTitleIndex + 1) % titles.length;
+                setTimeout(typeTitle, 1000);
+            }
+        }, 50);
+    }
+
+    typeTitle(); // Iniciar a animação
+});
